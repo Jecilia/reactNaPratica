2f8299a4203298a984282170ba5ba8fd99744038
Jecília Teixeira <111911408+Jecilia@users.noreply.github.com>
2024-03-26 23:47:17 +0100

    Update create-tag-form.tsx

--- src/components/create-tag-form.tsx
+++ src/components/create-tag-form.tsx
@@ -1,21 +1,37 @@
-import { Check } from 'lucide-react'
+import { Check, X } from 'lucide-react'
 import { Button } from './ui/button'
 
 export function CreateTagForm() {
   return (
-    <form action="">
-      <div>
-        <label>Tag name </label>
-        <input type="text" />
+    <form className="w-full space-y-6">
+      <div className="space-y-2">
+        <label htmlFor="name" className="text-sm font-medium block">
+          Tag name
+        </label>
+        <input
+          id="name"
+          type="text"
+          className="border border-zinc-800 rounded-lg px-3 py-2.5 bg-zinc-800/50 w-full text-sm"
+        />
       </div>
-      <div>
-        <label>Slug</label>
-        <input type="text" readOnly />
+      <div className="space-y-2">
+        <label htmlFor="slug" className="text-sm font-medium block">
+          Slug
+        </label>
+        <input
+          id="slug"
+          type="text"
+          readOnly
+          className="border border-zinc-800 rounded-lg px-3 py-2.5 bg-zinc-800/50 w-full text-sm"
+        />
       </div>
-      <div>
-        <Button>Cancel</Button>
-        <Button type="submit">
-          <Check />
+      <div className="flex items-center justify-end gap-2">
+        <Button>
+          <X className="size-3" />
+          Cancel
+        </Button>
+        <Button type="submit" className="bg-teal-400 text-teal-900">
+          <Check className="size-3" />
           Save
         </Button>
       </div>
