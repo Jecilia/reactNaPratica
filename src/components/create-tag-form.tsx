c3b2ac56b6e28a58de0a1e42a7dda6863fcab4c8
Jecília Teixeira <111911408+Jecilia@users.noreply.github.com>
2024-03-27 23:26:13 +0100

    create tag form

--- src/components/create-tag-form.tsx
+++ src/components/create-tag-form.tsx
@@ -1,14 +1,33 @@
 import { Check, X } from 'lucide-react'
 import { Button } from './ui/button'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+import * as Dialog from '@radix-ui/react-dialog'
 
+const createTagSchema = z.object({
+  name: z.string().min(3, { message: 'Minimun 3 characters.' }),
+  slug: z.string(),
+})
+
+type CreateTagSchema = z.infer<typeof createTagSchema>
 export function CreateTagForm() {
+  const { register, handleSubmit, watch } = useForm<CreateTagSchema>({
+    resolver: zodResolver(createTagSchema),
+  })
+  function createTag(data: CreateTagSchema) {
+    console.log(data)
+  }
+  const slug = watch('name')
+
   return (
-    <form className="w-full space-y-6">
+    <form onSubmit={handleSubmit(createTag)} className="w-full space-y-6">
       <div className="space-y-2">
         <label htmlFor="name" className="text-sm font-medium block">
           Tag name
         </label>
         <input
+          {...register('name')}
           id="name"
           type="text"
           className="border border-zinc-800 rounded-lg px-3 py-2.5 bg-zinc-800/50 w-full text-sm"
@@ -19,17 +38,22 @@ export function CreateTagForm() {
           Slug
         </label>
         <input
+          {...register('slug')}
           id="slug"
           type="text"
           readOnly
+          value={slug}
           className="border border-zinc-800 rounded-lg px-3 py-2.5 bg-zinc-800/50 w-full text-sm"
         />
       </div>
       <div className="flex items-center justify-end gap-2">
-        <Button>
-          <X className="size-3" />
-          Cancel
-        </Button>
+        <Dialog.Close asChild>
+          <Button>
+            <X className="size-3" />
+            Cancel
+          </Button>
+        </Dialog.Close>
+
         <Button type="submit" className="bg-teal-400 text-teal-900">
           <Check className="size-3" />
           Save
